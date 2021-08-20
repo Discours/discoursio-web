73e08f74dcd779c143ba96c12c7cda2c7304efa4
Untone <anton.rewin@gmail.com>
2021-08-20 05:07:17 +0300

    fix preprocess order

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,18 @@
 import { join } from 'path'
 import { readFileSync } from 'fs'
 import { cwd } from 'process'
-import sveltePre from 'svelte-preprocess'
+import { scss, globalStyle } from 'svelte-preprocess'
 import { typescript } from 'svelte-preprocess-esbuild'
 import { mdsvex } from 'mdsvex'
 import vercel from '@sveltejs/adapter-vercel'
 
-const { scss, globalStyle } = sveltePre
 const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json')))
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [mdsvex(), typescript(), scss(), globalStyle()],
+  preprocess: [globalStyle(), mdsvex(), typescript(), scss()],
 
   kit: {
     adapter: vercel(),
