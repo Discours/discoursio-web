3c86c7ac160dd82dc04adb0280eafc7e08016562
kvakazyambra <kvakazyambra@gmail.com>
2021-08-29 22:45:18 +0300

    Login page

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -39,168 +39,196 @@
 <svelte:head><title>Дискурс : Авторизация</title></svelte:head>
 
 <!-- svelte-ignore a11y-missing-attribute -->
-<div class="view">
-  <form class="auth">
-    <div class="tabs" style="width: 100%; height: 54px; margin-top: 16px;">
-      <div
-        class="login-tab-title half"
-        class:active={!create}
-        on:click={() => (create ? (create = false) : emailInput.focus())}
-      >
-        Вход
-      </div>
-      <div
-        class="register-tab-title half"
-        class:active={create}
-        on:click={() => (create ? emailInput.focus() : (create = true))}
-      >
-        Регистрация
-      </div>
-    </div>
-
-    <div style="width: 100%; height: 33px; margin-top: 16px;">
-      <div class="rowflex" style="justify-content: center;">
-        <div style="width: 100%;">
-          <input
-            autocomplete="username"
-            class="login-input"
-            bind:value={$auth.email}
-            bind:this={emailInput}
-            type="text"
-            placeholder="Ваша почта"
-          />
+<div class="row view">
+  <div class="col-sm-6 d-md-none login-image"></div>
+  <form class="col-sm-6 auth">
+    <div class="auth__inner">
+      <div class="tabs">
+        <div
+          class:active={!create}
+          on:click={() => (create ? (create = false) : emailInput.focus())}
+        >
+          Вход
+        </div>
+        <div
+          class:active={create}
+          on:click={() => (create ? emailInput.focus() : (create = true))}
+        >
+          Регистрация
         </div>
       </div>
-    </div>
 
-    <div style="width: 100%; height: 33px; margin-top: 16px;">
-      <div class="rowflex" style="justify-content: center;">
-        <div style="width: 100%;">
-          <input
-            autocomplete="current-password"
-            class="password-input"
-            bind:value={$auth.password}
-            type="password"
-            placeholder="Пароль"
-          />
+      <input
+        autocomplete="username"
+        bind:value={$auth.email}
+        bind:this={emailInput}
+        type="text"
+        placeholder="Ваша почта"
+      />
+
+      <input
+        autocomplete="current-password"
+        bind:value={$auth.password}
+        type="password"
+        placeholder="Пароль"
+      />
+
+      <div class="auth-actions">
+        <div>
+          <input id="remember" type="checkbox" bind:checked={remember} />
+          <label for="remember">Запомнить меня</label>
+        </div>
+        <div>
+          <a href="/resetpassword">Забыли пароль?</a>
         </div>
       </div>
-    </div>
 
-    <div style="width: 100%; margin-top: 16px;">
-      <div class="half">
-        <input type="checkbox" bind:checked={remember} />
-        <span style="line-height: 20px;">Запомнить</span>
-      </div>
-      <div class="half" style="color: rgba(158.93, 161.32, 167.47, 1);">
-        <a href="/resetpassword">Сброс пароля</a>
-      </div>
-    </div>
-
-    <div style="width: 100%; height: 33px; margin-top: 16px;">
-      <div class="submitbtn" on:click={create ? register : login}>
-        {create ? 'Создать аккаунт' : 'Войти'}
-      </div>
-    </div>
-    {#if useSocial}[v]{:else}[&nbsp;]{/if}
-    <a href="/login" on:click={() => (useSocial = !useSocial)}>
       <div>
-        {'Использовать учётную запись в соцсети'}
+        <button class="button submitbtn" on:click={create ? register : login}>
+          {create ? 'Создать аккаунт' : 'Войти'}
+        </button>
       </div>
-    </a>
-    {#if useSocial}
-      <div
-        style="width: 100%; height: 330px; margin-top: 16px;"
-        class="social-provider"
-      >
-        <div class="social">
-          <AuthVk
-            apiId={VK_APP_ID}
-            on:auth-success={providerSuccess}
-            on:auth-failure={providerFailure}
-          />
-          <AuthFacebook
-            appId={FACEBOOK_APP_ID}
-            on:auth-success={providerSuccess}
-            on:auth-failure={providerFailure}
-          />
+
+      {#if useSocial}[v]{:else}[&nbsp;]{/if}
+      <a href="/login" on:click={() => (useSocial = !useSocial)}>
+        <div>
+          {'Использовать учётную запись в соцсети'}
         </div>
-      </div>
-    {/if}
+      </a>
+      {#if useSocial}
+        <div
+          style="width: 100%; height: 330px; margin-top: 16px;"
+          class="social-provider"
+        >
+          <div class="social">
+            <AuthVk
+              apiId={VK_APP_ID}
+              on:auth-success={providerSuccess}
+              on:auth-failure={providerFailure}
+            />
+            <AuthFacebook
+              appId={FACEBOOK_APP_ID}
+              on:auth-success={providerSuccess}
+              on:auth-failure={providerFailure}
+            />
+          </div>
+        </div>
+      {/if}
+    </div>
   </form>
 </div>
 
 <style lang="scss">
-  div {
+  :global(#svelte) {
     display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    min-width: 10%;
-  }
-  .view {
-    width: 100%;
-    height: 100%;
-    display: inline-flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-  }
-  .tabs {
-    font-size: xx-large;
-  }
-  .rowflex {
-    flex: 1 1 0%;
-    height: 100%;
-  }
-  .password-input,
-  .login-input {
-    flex: 1 1 0%;
-    font-size: 17px;
-    line-height: 24px;
-    border: none;
-    outline: none;
-    color: var(--danger-color);
-  }
-  .register-tab-title,
-  .login-tab-title {
-    height: 40px;
     flex-direction: column;
-    align-items: center;
-    cursor: pointer;
-    pointer-events: all;
-    color: gray;
+    min-height: 100vh;
   }
 
-  .half {
-    width: 50%;
+  :global(main) {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
   }
 
-  .active {
-    color: black;
+  :global(.subnavigation) {
+    margin: 2rem auto !important;
+    width: 100%;
+  }
+
+  .view {
+    flex: 1;
+    margin: 0 auto;
+    max-width: 1440px;
+    width: 100%;
+
+    input {
+      @include font-size(1.7rem);
+    }
+  }
+
+  .login-image {
+    background: url('/static/auth-page.jpg') center no-repeat;
+    background-size: cover;
+
+    @include media-breakpoint-down(sm) {
+      display: none;
+    }
+  }
+
+  .tabs {
+    display: flex;
+    font-weight: 700;
+    @include font-size(2.6rem);
+    margin-bottom: 1.6rem;
+
+    & > div {
+      cursor: pointer;
+      color: #696969;
+
+      &:first-child {
+        margin-right: 2.4rem;
+      }
+    }
+
+    .active {
+      border-bottom: 2px solid;
+      color: #000;
+      cursor: default;
+    }
+  }
+
+  .auth-actions {
+    justify-content: space-between;
+    margin-bottom: 1.6rem;
+
+    @include media-breakpoint-up(md) {
+      display: flex;
+    }
+
+    a {
+      color: #9fa1a7;
+    }
+
+    & > div {
+      margin-bottom: 0.8rem;
+    }
   }
 
   .auth {
-    display: inline-flex;
-    flex-direction: column;
-    min-width: 45%;
-  }
-  .submitbtn {
-    cursor: pointer;
-    pointer-events: all;
-    padding: 16px;
     display: flex;
-    user-select: none;
-    font-size: 25px;
-    text-align: center;
-    width: 100%;
-    font-weight: 700;
-    color: rgba(158.93, 161.32, 167.47, 1);
+    flex-direction: column;
+    justify-content: center;
+    padding: $container-padding-x;
+
+    @include media-breakpoint-up(lg) {
+      padding: 3rem 6.8rem;
+    }
   }
+
+  .submitbtn {
+    color: #9fa1a7;
+    display: block;
+    font-weight: 700;
+    @include font-size(2rem);
+    padding: 1.6rem;
+    pointer-events: all;
+    text-align: center;
+    user-select: none;
+    width: 100%;
+  }
+
   input[type='text'],
   input[type='password'] {
-    border-bottom: 1px solid rgb(162, 162, 162);
+    border: 2px solid #e8e8e8;
+    border-radius: 2px;
+    display: block;
+    font-family: 'Muller', Arial, Helvetica, sans-serif;
+    margin-bottom: 1.6rem;
+    padding: 1.5rem 1.2rem;
+    width: 100%;
   }
+
   .social {
     background-color: white !important;
     padding: 1em;
