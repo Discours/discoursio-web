b0e5b3cb2dc6b5d459e94eb35425aebff5fd7fe4
Untone <anton.rewin@gmail.com>
2021-09-09 12:25:49 +0300

    fix preprocessor imports

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { join } from 'path'
 import { readFileSync } from 'fs'
 import { cwd } from 'process'
-import { scss, globalStyle } from 'svelte-preprocess'
 import { typescript } from 'svelte-preprocess-esbuild'
 import { mdsvex } from 'mdsvex'
 import vercel from '@sveltejs/adapter-vercel'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const { scss, globalStyle } = require('svelte-preprocess')
 
 const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json')))
-
+const preprocess = [
+  globalStyle(),
+  mdsvex(),
+  typescript(),
+  scss({
+    // https://github.com/sveltejs/svelte-preprocess/blob/main/docs/getting-started.md#31-prepending-content
+    prependData: `@import 'src/styles/_imports.scss';`,
+    // Docs say renderSync is faster for Dart Sass which I am using
+    // https://github.com/sveltejs/svelte-preprocess/blob/main/docs/preprocessing.md#scss-sass
+    renderSync: true,
+    // Dart Sass recognizes 'expanded' and 'compressed'
+    outputStyle: 'expanded',
+  }),
+]
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [
-    globalStyle(),
-    mdsvex(),
-    typescript(),
-    scss({
-      // https://github.com/sveltejs/svelte-preprocess/blob/main/docs/getting-started.md#31-prepending-content
-      prependData: `@import 'src/styles/_imports.scss';`,
-      // Docs say renderSync is faster for Dart Sass which I am using
-      // https://github.com/sveltejs/svelte-preprocess/blob/main/docs/preprocessing.md#scss-sass
-      renderSync: true,
-      // Dart Sass recognizes 'expanded' and 'compressed'
-      outputStyle: 'expanded',
-    }),
-  ],
+  preprocess,
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   compilerOptions: { cssHash: ({ hash, css }) => hash(css) },
   kit: {
