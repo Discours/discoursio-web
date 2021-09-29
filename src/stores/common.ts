56fccfca89714bb180fd14101b43d34c0875d63e
Untone <anton.rewin@gmail.com>
2021-09-29 13:10:45 +0300

    graphql client path fix

--- src/stores/common.ts
+++ src/stores/common.ts
@@ -1,14 +1,11 @@
 import { writable } from 'svelte/store'
 import type { Writable } from 'svelte/store'
-import { GraphQLClient } from 'graphql-request'
 
 export enum Locale {
   RU = 'ru',
   EN = 'en',
 }
 
-export const graphql: Writable<GraphQLClient> = writable(
-  new GraphQLClient('/graphql') // <host>/graphql is API endpoint
-)
+export const graphql: Writable<GraphQLClient> = writable()
 export const loaded: Writable<boolean> = writable(false)
 export const lang: Writable<Locale> = writable(Locale.RU)
