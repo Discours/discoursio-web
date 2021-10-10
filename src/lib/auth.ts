2fa62bb835e4841d360b02aff6b9f529197a1193
Untone <anton.rewin@gmail.com>
2021-10-10 18:42:19 +0300

    auth utils

---
+++ src/lib/auth.ts
@@ -0,0 +1,19 @@
+//import Cookies from 'js-cookie'
+
+export const isLoggedIn = (): boolean => {
+  //') as string) !== undefined || false;
+  // or return fake true for testing purposes
+  return true
+}
+
+export const routeProtect = (
+  base: Record<string, unknown> = {}
+): Record<string, unknown> => {
+  if (!isLoggedIn()) {
+    return {
+      redirect: '/app/auth',
+      status: 303,
+    }
+  }
+  return base
+}
