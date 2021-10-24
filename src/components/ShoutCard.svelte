53899c3591d2470cdeef23ff8e8505a90415c6d8
Tony Rewin <anton.rewin@gmail.com>
2021-10-24 16:57:44 +0300

    cols is back

--- src/components/ShoutCard.svelte
+++ src/components/ShoutCard.svelte
@@ -69,9 +69,9 @@
         margin-bottom: 2.4rem;
       }
 
-      //.shout-card__cover-container {
-        // @include make-col(4);
-      //}
+      .shout-card__cover-container {
+        @include make-col(4);
+      }
 
       .shout-card__cover {
         margin-bottom: 0;
