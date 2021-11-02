b933de316460862d7097d860a1c1d43f4272c2eb
Untone <anton.rewin@gmail.com>
2021-11-02 16:43:59 +0300

    discours-editor isolated

---
+++ src/components/DiscoursEditor.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	let Editor
+	export let props
+
+	onMount(
+		async () => (Editor = await import('discours-editor/src/Editor.svelte'))
+	)
+</script>
+
+<div><Editor {props} /></div>
--- src/routes/create.svelte
+++ src/routes/create.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	// import Editor from '../components/Editor.svelte'
+	import DiscoursEditor from '../components/DiscoursEditor.svelte'
 	import type { Shout } from '../graphql/codegen'
 
 	let shout: Shout
 </script>
 
 <svelte:head><title>Дискурс : Редакция</title></svelte:head>
-<!--Editor /--> Editor is under construction
+<DiscoursEditor props={{}} />
+<div style="margin: 3vw;">
+	Инструмент совместного редактирования находится в разработке.
+</div>
