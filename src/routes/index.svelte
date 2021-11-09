a150788f2d399607f097e91df88f2717eba67457
Untone <anton.rewin@gmail.com>
2021-11-09 20:13:22 +0300

    fiix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -28,17 +28,17 @@
 		console.log(r)
 		$shoutslist = r.recents
 
-		// r = await $api.request(TOP_MONTH, { limit: 100 })
-		// console.log(r.topMonth)
-		// r = await $api.request(TOP_OVERALL, { limit: 100 })
-		// console.log(r.topOverall)
-		// $shoutslist = [...$shouts.recents, ...$shouts.topMonth, ...$shouts.topOverall]
+		r = await $api.request(TOP_MONTH, { limit: 100 })
+		console.log(r.topMonth)
+		r = await $api.request(TOP_OVERALL, { limit: 100 })
+		console.log(r.topOverall)
+		// $shoutslist = [...$shouts.recents], ...$shouts.topMonth, ...$shouts.topOverall]
 		console.log(`homepage: loaded ${$shoutslist.length} shouts`)
 	})
 
 	const recent = () => {
 		// returns recently updated articles (default)
-		return $shouts.recents
+		return $shoutslist
 	}
 
 	const topViewed = () => {
@@ -83,206 +83,207 @@
 </script>
 
 <svelte:head><title>Дискурс : Главная</title></svelte:head>
+{#if $shoutslist}
+	<div class="home">
+		{#if $topicslist.length > 0}
+			<NavTopics slugs={Object.keys($topics).sort()} />
+		{/if}
 
-<div class="home">
-	{#if $topicslist.length > 0}
-		<NavTopics slugs={Object.keys($topics).sort()} />
-	{/if}
-
-	<div class="floor floor--1">
-		<div class="wide-container row">
-			<div class="col-md-3">
-				{#each $shoutslist.slice(0, 2) as shout}
-					<ShoutCard {shout} />
-				{/each}
-			</div>
-			<div class="col-md-6">
-				{#each $shoutslist.slice(2, 3) as shout}
-					<ShoutCard {shout} />
-				{/each}
-			</div>
-			<div class="col-md-3">
-				{#each $shoutslist.slice(3, 5) as shout}
-					<ShoutCard {shout} />
-				{/each}
-			</div>
-		</div>
-	</div>
-
-	<div class="floor floor--2">
-		<div class="wide-container row">
-			<div class="col-md-6">
-				{#each $shoutslist.slice(5, 6) as article}
-					<ShoutCard shout={article} />
-				{/each}
-			</div>
-			<div class="col-md-6">
-				<h4>Самое читаемое</h4>
-				{#each topViewed().slice(0, 4) as article}
-					<ShoutCard shout={article} />
-				{/each}
-			</div>
-		</div>
-	</div>
-
-	<div class="floor floor--3">
-		<div class="wide-container row">
-			{#each $shoutslist.slice(10, 13) as article}
-				<div class="col-md-4">
-					<ShoutCard shout={article} />
-				</div>
-			{/each}
-		</div>
-	</div>
-
-	<div class="floor floor--important">
-		<div class="wide-container row">
-			<h2 class="col-12"><span>Важное</span></h2>
-			{#each topCommented().slice(0, 4) as article}
-				<div class="col-md-4">
-					<ShoutCard shout={article} />
-				</div>
-			{/each}
-		</div>
-	</div>
-
-	<div class="floor floor--5">
-		<div class="wide-container row">
-			{#each $shoutslist.slice(16, 18) as article}
-				<div class="col-md-4">
-					<ShoutCard shout={article} />
-				</div>
-			{/each}
-		</div>
-	</div>
-
-	<div class="floor floor--6">
-		<div class="wide-container row">
-			<div class="col-md-8">
-				<ShoutCard shout={$shoutslist[19]} />
-			</div>
-			<div class="col-md-4">
-				<h4>Авторы месяца</h4>
-
-				{#each topAuthors().slice(0, 4) as user}
-					<UserCard {user} hasSubscribeButton={true} />
-				{/each}
-
-				<button class="button">Еще авторы</button>
-			</div>
-		</div>
-	</div>
-
-	<div class="floor floor--7">
-		<div class="wide-container row">
-			<h2 class="col-12">Коротко</h2>
-			{#each recent().slice(0, 4) as article}
-				<div class="col-md-6 col-lg-3">
-					<ShoutCard shout={article} />
-				</div>
-			{/each}
-		</div>
-	</div>
-
-	<div class="floor floor--important">
-		<div class="wide-container row">
-			<h2 class="col-12"><span>Избранное</span></h2>
-			{#each topRated().slice(0, 4) as article}
+		<div class="floor floor--1">
+			<div class="wide-container row">
 				<div class="col-md-3">
-					<ShoutCard shout={article} />
+					{#each $shoutslist.slice(0, 2) as shout}
+						<ShoutCard {shout} />
+					{/each}
 				</div>
-			{/each}
+				<div class="col-md-6">
+					{#each $shoutslist.slice(2, 3) as shout}
+						<ShoutCard {shout} />
+					{/each}
+				</div>
+				<div class="col-md-3">
+					{#each $shoutslist.slice(3, 5) as shout}
+						<ShoutCard {shout} />
+					{/each}
+				</div>
+			</div>
 		</div>
-	</div>
 
-	<div class="floor floor--9">
-		<div class="wide-container row">
-			<div class="col-md-4">
-				<h4>Популярные сообщества</h4>
-				{#each $communitieslist as community}
-					<UserCommunity slug={community['slug']} />
+		<div class="floor floor--2">
+			<div class="wide-container row">
+				<div class="col-md-6">
+					{#each $shoutslist.slice(5, 6) as article}
+						<ShoutCard shout={article} />
+					{/each}
+				</div>
+				<div class="col-md-6">
+					<h4>Самое читаемое</h4>
+					{#each topViewed().slice(0, 4) as article}
+						<ShoutCard shout={article} />
+					{/each}
+				</div>
+			</div>
+		</div>
+
+		<div class="floor floor--3">
+			<div class="wide-container row">
+				{#each $shoutslist.slice(10, 13) as article}
+					<div class="col-md-4">
+						<ShoutCard shout={article} />
+					</div>
 				{/each}
 			</div>
-			<div class="col-md-8">
-				<ShoutCard shout={$shoutslist[28]} />
+		</div>
+
+		<div class="floor floor--important">
+			<div class="wide-container row">
+				<h2 class="col-12"><span>Важное</span></h2>
+				{#each $shoutslist.slice(0, 4) as article}
+					<div class="col-md-4">
+						<ShoutCard shout={article} />
+					</div>
+				{/each}
 			</div>
 		</div>
-	</div>
 
-	<div class="floor floor--10">
-		<div class="wide-container row">
-			{#each $shoutslist.slice(20, 23) as article}
+		<div class="floor floor--5">
+			<div class="wide-container row">
+				{#each $shoutslist.slice(16, 18) as article}
+					<div class="col-md-4">
+						<ShoutCard shout={article} />
+					</div>
+				{/each}
+			</div>
+		</div>
+
+		<div class="floor floor--6">
+			<div class="wide-container row">
+				<div class="col-md-8">
+					<ShoutCard shout={$shoutslist[19]} />
+				</div>
 				<div class="col-md-4">
-					<ShoutCard shout={article} />
-				</div>
-			{/each}
-		</div>
-	</div>
+					<h4>Авторы месяца</h4>
 
-	<div class="floor floor--2">
-		<div class="wide-container row">
-			<div class="col-md-6">
-				<ShoutCard shout={$shoutslist[27]} />
+					{#each topAuthors().slice(0, 4) as user}
+						<UserCard {user} hasSubscribeButton={true} />
+					{/each}
+
+					<button class="button">Еще авторы</button>
+				</div>
 			</div>
-			<div class="col-md-6">
-				<h4>Самое обсуждаемое</h4>
-				{#each $shoutslist.slice(21, 24) as article}
-					<ShoutCard shout={article} />
+		</div>
+
+		<div class="floor floor--7">
+			<div class="wide-container row">
+				<h2 class="col-12">Коротко</h2>
+				{#each recent().slice(0, 4) as article}
+					<div class="col-md-6 col-lg-3">
+						<ShoutCard shout={article} />
+					</div>
+				{/each}
+			</div>
+		</div>
+
+		<div class="floor floor--important">
+			<div class="wide-container row">
+				<h2 class="col-12"><span>Избранное</span></h2>
+				{#each topRated().slice(0, 4) as article}
+					<div class="col-md-3">
+						<ShoutCard shout={article} />
+					</div>
+				{/each}
+			</div>
+		</div>
+
+		<div class="floor floor--9">
+			<div class="wide-container row">
+				<div class="col-md-4">
+					<h4>Популярные сообщества</h4>
+					{#each $communitieslist as community}
+						<UserCommunity slug={community['slug']} />
+					{/each}
+				</div>
+				<div class="col-md-8">
+					<ShoutCard shout={$shoutslist[28]} />
+				</div>
+			</div>
+		</div>
+
+		<div class="floor floor--10">
+			<div class="wide-container row">
+				{#each $shoutslist.slice(20, 23) as article}
+					<div class="col-md-4">
+						<ShoutCard shout={article} />
+					</div>
+				{/each}
+			</div>
+		</div>
+
+		<div class="floor floor--2">
+			<div class="wide-container row">
+				<div class="col-md-6">
+					<ShoutCard shout={$shoutslist[27]} />
+				</div>
+				<div class="col-md-6">
+					<h4>Самое обсуждаемое</h4>
+					{#each $shoutslist.slice(21, 24) as article}
+						<ShoutCard shout={article} />
+					{/each}
+				</div>
+			</div>
+		</div>
+
+		<div class="floor floor--teaser">
+			<div class="wide-container row">
+				<div class="col-md-12">
+					<ShoutCard shout={$shoutslist[33]} />
+				</div>
+			</div>
+		</div>
+
+		<div class="floor floor--11">
+			<div class="wide-container row">
+				<div class="col-md-4">
+					<ShoutCard shout={$shoutslist[11]} />
+				</div>
+				<div class="col-md-8">
+					<ShoutCard shout={$shoutslist[23]} />
+				</div>
+			</div>
+		</div>
+
+		<div class="floor floor--12">
+			<div class="wide-container row">
+				<div class="col-md-4">
+					<h4>Культура</h4>
+					{#each $shoutslist
+						.filter((s) => (s['topics'] || []).includes('culture'))
+						.slice(0, 4) as article}
+						<ShoutCard shout={article} />
+					{/each}
+				</div>
+				<div class="col-md-4">
+					<ShoutCard shout={$shoutslist[33]} />
+				</div>
+				<div class="col-md-4">
+					<ShoutCard shout={$shoutslist[22]} />
+				</div>
+			</div>
+		</div>
+
+		<DiscoursBanner />
+
+		<div class="floor floor--10">
+			<div class="wide-container row">
+				{#each $shoutslist.slice(26, 27) as article}
+					<div class="col-md-426">
+						<ShoutCard shout={article} />
+					</div>
 				{/each}
 			</div>
 		</div>
 	</div>
-
-	<div class="floor floor--teaser">
-		<div class="wide-container row">
-			<div class="col-md-12">
-				<ShoutCard shout={$shoutslist[33]} />
-			</div>
-		</div>
-	</div>
-
-	<div class="floor floor--11">
-		<div class="wide-container row">
-			<div class="col-md-4">
-				<ShoutCard shout={$shoutslist[11]} />
-			</div>
-			<div class="col-md-8">
-				<ShoutCard shout={$shoutslist[23]} />
-			</div>
-		</div>
-	</div>
-
-	<div class="floor floor--12">
-		<div class="wide-container row">
-			<div class="col-md-4">
-				<h4>Культура</h4>
-				{#each $shoutslist
-					.filter((s) => (s['topics'] || []).includes('culture'))
-					.slice(0, 4) as article}
-					<ShoutCard shout={article} />
-				{/each}
-			</div>
-			<div class="col-md-4">
-				<ShoutCard shout={$shoutslist[33]} />
-			</div>
-			<div class="col-md-4">
-				<ShoutCard shout={$shoutslist[22]} />
-			</div>
-		</div>
-	</div>
-
-	<DiscoursBanner />
-
-	<div class="floor floor--10">
-		<div class="wide-container row">
-			{#each $shoutslist.slice(26, 27) as article}
-				<div class="col-md-426">
-					<ShoutCard shout={article} />
-				</div>
-			{/each}
-		</div>
-	</div>
-</div>
+{/if}
 
 <style lang="scss">
 	.floor {
