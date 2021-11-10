3e82ca5462b4956515df71a84883bb44a0a87b60
Untone <anton.rewin@gmail.com>
2021-11-10 21:28:07 +0300

    authorized

--- src/routes/authorized.ts
+++ src/routes/authorized.ts
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { api as graphql } from '../stores/common'
+import { token } from '../stores/auth'
 import { get as getStore } from 'svelte/store'
 import { GET_ME } from '../graphql/queries'
 
 export async function get(req) {
-    const token = req.query.get('token')
+    const got = req.query.get('token')
+    token.set(got)
     const api = getStore(graphql)
-	const session = await api.request(GET_ME, { token })
+	const session = await api.request(GET_ME)
     return { session }
 }
