aa91852c02a8646923daa7f21f0bb677dc373536
Untone <anton.rewin@gmail.com>
2021-11-12 20:38:14 +0300

    enjoy

--- src/stores/zine.ts
+++ src/stores/zine.ts
@@ -1,18 +1,19 @@
 import { writable } from 'svelte/store'
 import type { Writable } from 'svelte/store'
-import type { Shout, User, Topic, Community } from '../graphql/codegen'
+import type { User, Topic, Community } from '../graphql/codegen'
+import type { Shout } from '../lib/types'
 
-export const topMonth: Writable<Array<Shout | Partial<Shout>>> = writable([])
-export const topOverall: Writable<Array<Shout | Partial<Shout>>> = writable([])
-export const recents: Writable<Array<Shout | Partial<Shout>>> = writable([])
+export const topMonth: Writable<Shout[]> = writable([])
+export const topOverall: Writable<Shout[]> = writable([])
+export const recents: Writable<Array<Shout[]>> = writable([])
 
-export const authorslist: Writable<Array<User | Partial<User>>> = writable([])
+export const authorslist: Writable<Array<Partial<User>>> = writable([])
 export const communitieslist: Writable<Array<Community | Partial<Community>>> =
 	writable([])
 export const shoutslist: Writable<Array<Shout | Partial<Shout>>> = writable([])
 export const topicslist: Writable<Topic[]> = writable([])
 
-export const shouts: Writable<{ [slug: string]: any }> = writable({})
+export const shouts: Writable<{ [slug: string]: Shout }> = writable({})
 export const topics: Writable<{ [slug: string]: Topic }> = writable({})
 export const authors: Writable<{ [slug: string]: User | Partial<User> }> =
 	writable({})
