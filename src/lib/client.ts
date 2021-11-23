cf6b1076aa01dd51dfd5ecff6072e0d9c185ccf5
Tony Rewin <anton.rewin@gmail.com>
2021-11-23 18:59:36 +0300

    endpoint

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request'
 
-const GRAPHQL_ENDPOINT = <string>import.meta.env.GRAPHQL_ENDPOINT || 'http://localhost:8080'
+const GRAPHQL_ENDPOINT = 'https://build.discours.io/graphql'
 
 export const client = new GraphQLClient(GRAPHQL_ENDPOINT)
