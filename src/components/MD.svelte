17811276ea7a587cd48a482964e765dbc5b2d3ca
Tony Rewin <anton.rewin@gmail.com>
2021-11-24 09:43:36 +0300

    markdown component improve

--- src/components/MD.svelte
+++ src/components/MD.svelte
@@ -1,22 +1,22 @@
-<script>
+<script context="module">
 	import MD from 'markdown-it'
-	// import hl from 'markdown-it-highlight'
 	import mdanch from 'markdown-it-anchor'
 	import mark from 'markdown-it-mark'
 	import implicit from 'markdown-it-implicit-figures'
 	import mdcustom from 'markdown-it-container'
+	// import hl from 'markdown-it-highlight'
 
 	const mit = MD()
 	mit.use(mdanch)
-	// mit.use(hl)
 	mit.use(mark)
 	mit.use(mdcustom)
-
 	mit.use(implicit, {
 		dataType: false, // <figure data-type="image">
 		figcaption: true // <figcaption>alternative text</figcaption>
 	})
-
+	// mit.use(hl)
+</script>
+<script>
     export let body
 </script>
 
