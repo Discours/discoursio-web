5e85a7eaba3e1868e7ae888da253bd5a11879680
kvakazyambra <kvakazyambra@gmail.com>
2021-12-12 22:17:27 +0300

    Topics page

--- src/routes/topic/index.svelte
+++ src/routes/topic/index.svelte
@@ -12,12 +12,207 @@
 	}
 </script>
 <script lang="ts">
+	import {onMount} from "svelte";
+
+	const groupBy = (arr) => {
+		let firstLetter = null;
+
+	  return arr.reduce((acc, currentValue) => {
+			const currentLetter = currentValue.title.slice(0, 1);
+	    if (!acc[currentLetter]) {
+				firstLetter = currentLetter;
+	      acc[firstLetter] = [];
+	    }
+	    acc[currentLetter].push(currentValue);
+	    return acc;
+	  }, {});
+	}
+
 	export let topics: Partial<Topic>[]
+
+	const viewSwitcherItems = {
+		popular: 'Популярные',
+		discussed: 'Обсуждаемые',
+		alphabet: 'По алфавиту',
+	}
+
+	let currentSwitcherItem = null;
+
+	const switchView = (view) => {
+		currentSwitcherItem = view;
+	};
+
+	onMount(() => {
+		currentSwitcherItem = window.location.hash.replace('#', '');
+	});
+
+	const topicsGroupedByAlphabet = groupBy(topics.slice(0));
+	const sortedKeys = Object.keys(topicsGroupedByAlphabet).sort();
+
+	sortedKeys.forEach(letter => {
+		topicsGroupedByAlphabet[letter] =
+			topicsGroupedByAlphabet[letter].sort((a, b) => {
+				if (a.title > b.title) {
+					return 1;
+				} else if(a.title < b.title) {
+					return -1;
+				}
+				return 0;
+			});
+	});
 </script>
 
-<h2>Темы</h2>
-{#each topics as t}
-	<div class="topic">
-		<div class="topic-title">{t.title}</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-9">
+			<h1>Темы</h1>
+			<p>Подпишитесь на интересующие вас темы, чтобы настроить вашу персональную
+				летну и моментально узнавать о новых публикациях и обсуждениях</p>
+		</div>
 	</div>
-{/each}
+
+	<div class="row">
+		<div class="col">
+			<ul class="view-switcher">
+				{#each Object.entries(viewSwitcherItems) as [key, title]}
+					<li class:selected="{currentSwitcherItem === key}"
+							on:click="{() => switchView(key)}">
+						<a href="#{key}">{title}</a>
+					</li>
+				{/each}
+			</ul>
+
+			{#if currentSwitcherItem === 'alphabet'}
+				{#each sortedKeys as letter}
+					<div class="group">
+						<h2>{letter}</h2>
+
+						<div class="container">
+							<div class="row">
+								{#each topicsGroupedByAlphabet[letter] as topic}
+									<div class="topic col-3">
+										<div class="topic-title">
+											<a href="/{topic.slug}">{topic.title}</a>
+										</div>
+									</div>
+								{/each}
+							</div>
+						</div>
+					</div>
+				{/each}
+			{/if}
+
+			{#if currentSwitcherItem === 'popular'}
+				<div class="popular">
+					{#each topics as topic}
+						<div class="topic row">
+							<div class="col-md-7">
+								<div class="topic-title">
+									<a href="/{topic.slug}">{topic.title}</a>
+								</div >
+								<p class="topic-description">Цикл монологов об эмиграции из
+									России в Финляндию: о жизни до переезда, причинах расставания с родиной, поиске своего места в новой стране и о том, какие трудности и радости ждут русского человека за линией Маннергейма.</p>
+								<div class="topic-details">
+									<span class="topic-details__item">36 публикаций</span>
+									<span class="topic-details__item">24563 просмотров</span>
+									<span class="topic-details__item">12 авторов</span>
+									<span class="topic-details__item">355 подписчиков</span>
+								</div>
+							</div>
+							<div class="col-md-3 offset-md-2">
+								<button>Подписаться</button>
+							</div>
+						</div>
+					{/each}
+				</div>
+			{/if}
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.view-switcher {
+		font-weight: bold;
+		list-style: none;
+		margin: 0 0 1.5em;
+		padding: 0;
+
+		li {
+			display: inline-block;
+			margin-right: 1em;
+		}
+
+		a {
+			border-bottom: 2px solid #fff;
+			color: #696969;
+		}
+
+		.selected {
+			a {
+				border-color: #000;
+				color: #000;
+				cursor: default;
+			}
+		}
+	}
+
+	.topic {
+		margin-bottom: 2.4rem;
+
+		.popular & {
+			margin-bottom: 6.4rem;
+		}
+
+		:global(button) {
+			background: #f6f6f6;
+			border: none;
+			cursor: pointer;
+			margin-top: 0.8rem;
+			padding: 0.6rem 1.2rem;
+		}
+	}
+
+	.topic-title {
+		.popular & {
+			font-weight: bold;
+			@include font-size(2.6rem);
+		}
+
+		&:first-letter {
+			text-transform: capitalize;
+		}
+	}
+
+	.topic-description {
+		color: #696969;
+	}
+
+	.topic-details {
+		@include font-size(1.7rem);
+		color: #9fa1a7;
+	}
+
+	.topic-details__item {
+		margin-right: 1.6rem;
+
+		&:last-child {
+			margin-right: 0;
+		}
+	}
+
+	.group {
+		font-weight: bold;
+		margin-bottom: 9.6rem;
+
+		h2 {
+			margin-bottom: 3.2rem;
+			text-transform: capitalize;
+		}
+
+		.container {
+			margin-left: $grid-gutter-width;
+			padding-right: $container-padding-x;
+			width: auto;
+		}
+	}
+</style>
