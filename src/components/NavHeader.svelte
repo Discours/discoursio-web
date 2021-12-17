237e92c6ed61eb4bb5533cd567f7e07491eec543
kvakazyambra <kvakazyambra@gmail.com>
2021-12-18 01:14:24 +0300

    Responsive header navigation

--- src/components/NavHeader.svelte
+++ src/components/NavHeader.svelte
@@ -7,6 +7,13 @@
 
 	let res = ''
 	onMount(() => (res = window.location.pathname))
+	let isBurgerHidden = true
+	let body = null
+
+	const toggleBurger = () => {
+		isBurgerHidden = !isBurgerHidden
+		body.classList.toggle('fixed', !isBurgerHidden)
+	}
 
 	const MAIN_NAVIGATION = [
 		{
@@ -27,20 +34,27 @@
 			disabled: true
 		}
 	]
+
+	onMount(() => {
+		body = document.querySelector('body')
+	})
 </script>
 
 <!-- svelte-ignore a11y-missing-attribute -->
 <div class="wide-container">
-	<nav class="row header__inner">
-		<div class="main-logo col-auto"><a href="/">
-			<img src="/logo.svg" alt="Дискурс" width="175" height="32" />
-		</a></div>
+	<nav class="row header__inner" class:header__inner--fixed={!isBurgerHidden}>
+		<div class="main-logo col-auto">
+			<a href="/">
+				<img src="/logo.svg" alt="Дискурс" />
+			</a>
+		</div>
 
-		<ul class="col main-navigation text-xl inline-flex">
+		<ul class="col main-navigation text-xl inline-flex"
+				class:main-navigation--open={!isBurgerHidden}>
 			{#each MAIN_NAVIGATION as navItem}
 				<li class:selected={res === navItem.href}>
 					{#if res === navItem.href}
-						{navItem.title}
+						<strong>{navItem.title}</strong>
 					{:else if navItem.disabled}
 						{navItem.title}
 					{:else}
@@ -50,27 +64,54 @@
 					{/if}
 				</li>
 			{/each}
-			<li class="float-right">
-				<NavUser />
-			</li>
 		</ul>
+
+		<NavUser />
+
+		<div class="burger-container">
+			<div class="burger" class:burger--close={!isBurgerHidden} on:click={toggleBurger}>
+				<div></div>
+			</div>
+		</div>
 	</nav>
 </div>
 
 <style lang="scss">
 	.header__inner {
+		background: #fff;
 		flex-wrap: wrap;
+	}
+
+	.header__inner--fixed {
+		border-bottom: 4px solid #000;
+		left: 0;
+		position: fixed;
+		right: 0;
+		top: 0;
+		padding: 0 $container-padding-x;
+		z-index: 10;
 	}
 
 	.main-logo {
 		align-items: center;
 		display: inline-flex;
-		height: 80px;
-		padding-right: 3.2rem;
+		height: 70px;
+		padding: 0 $container-padding-x;
 		text-align: center;
+		position: relative;
+		z-index: 10;
+
+		@include media-breakpoint-up(lg) {
+			height: 80px;
+		}
 
 		img {
 			vertical-align: middle;
+			width: 100px;
+
+			@include media-breakpoint-up(lg) {
+				width: 175px;
+			}
 		}
 
 		a {
@@ -89,6 +130,24 @@
 		margin: 0;
 		padding: 0;
 
+		@include media-breakpoint-down(lg) {
+			background: #fff;
+			bottom: 0;
+			display: none;
+			font-size: 2.6rem;
+			font-weight: bold;
+			left: 0;
+			padding: $container-padding-x;
+			position: fixed;
+			top: 74px;
+			width: 100%;
+			z-index: 1;
+
+			li {
+				margin-bottom: 2.4rem;
+			}
+		}
+
 		li {
 			margin-right: 2.4rem;
 
@@ -105,9 +164,17 @@
 			}
 		}
 
-		.selected {
+		.selected strong {
 			border-bottom: 2px solid;
 			color: #000;
+		}
+	}
+
+	.main-navigation--open {
+		display: inline-flex;
+
+		@include media-breakpoint-down(lg) {
+			display: block !important;
 		}
 	}
 
@@ -116,4 +183,80 @@
 		text-align: right;
 		padding-right: 1vw;
 	}
+
+	.burger-container {
+		box-sizing: content-box;
+		display: inline-flex;
+		padding-left: 0;
+		width: 2.2rem;
+
+		@include media-breakpoint-up(sm) {
+			padding-left: divide($container-padding-x, 2);
+		}
+
+		@include media-breakpoint-up(md) {
+			display: none;
+		}
+	}
+
+	.burger {
+		background: #fff;
+		cursor: pointer;
+		height: 1.8rem;
+		display: inline-block;
+		position: relative;
+		vertical-align: middle;
+		width: 2.2rem;
+
+		> div,
+		&:after,
+		&:before {
+			background: #000;
+			content: '';
+			display: block;
+			height: 2px;
+			left: 0;
+			position: absolute;
+			width: 100%;
+		}
+
+		> div {
+			margin-top: -1px;
+			opacity: 1;
+			top: 50%;
+			transition: opacity 0.2s 0.1s;
+		}
+
+		&:after,
+		&:before {
+			transform-origin: center !important;
+			transition: top 0.3s, transform 0.3s;
+		}
+
+		&:after {
+			bottom: 0;
+		}
+
+		&:before {
+			top: 0;
+		}
+	}
+
+	.burger--close {
+		> div {
+			opacity: 0;
+			transition: opacity 0s;
+		}
+
+		&:after {
+			bottom: 0.8rem;
+			transform: rotate(-45deg);
+		}
+
+		&:before {
+			transform: rotate(45deg);
+			top: 0.8rem;
+		}
+	}
+
 </style>
