cecd662fc78c2a6d0e6efb4294803e16a81e1536
Tony <anton.rewin@gmail.com>
2022-01-10 19:25:01 +0300

    hooks2

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,23 +1,12 @@
 import { token } from "./stores/user"
 
-const subnames = ['topics', 'authors']
-
 export async function handle({ request, resolve }) {
 	const cookies: { [key: string]: string } = {}
 	const rhc = request.headers && request.headers.cookie
 	if (rhc) rhc.split('=').forEach((c) => (cookies[c[0]] = c[1].split(';')[0]))
-	subnames.forEach(async (s) => {
-		if(cookies[s]) cookies[s] = await JSON.parse(cookies[s] || '')
-	})
-	let subdomain = request.host && request.host.split('.')[0]
-	if (subdomain === 'discours' || subdomain === 'new') subdomain = ''
-	token.set(cookies['token'])
-	if(subdomain) request.locals = { subdomain }
-	if(Object.keys(cookies).length > 0) {
-		console.debug(cookies)
-		request.session = cookies
-	}
-	// console.debug(request)
+	// let subdomain = request.host && request.host.split('.')[0]
+	// if (subdomain === 'discours' || subdomain === 'new') subdomain = ''
+	cookies['token'] ?? token.set(cookies['token'])
 	const response = await resolve(request)
 	return response
 }
