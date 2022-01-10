bd5a78c5038c183d5d0c473f0f385e145d844e72
Tony <anton.rewin@gmail.com>
2022-01-10 22:16:45 +0300

    fix banner

--- src/components/DiscoursBanner.svelte
+++ src/components/DiscoursBanner.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="discours-banner">
-	<a href="/" class="wide-container row">
+	<div class="wide-container row">
 		<div class="discours-banner__content col-md-5">
 			<h3>Дискурс существует благодаря вашему участию</h3>
 			<p>
@@ -17,7 +17,7 @@
 		<div class="col-md-6 offset-md-1 discours-banner__image">
 			<img src="/discours-banner.jpg" alt="Дискурс" />
 		</div>
-	</a>
+	</div>
 </div>
 
 <style lang="scss">
