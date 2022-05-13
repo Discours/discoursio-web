e9299958fded300e3ac7d49a9c4ec67c0752c766
kvakazyambra <kvakazyambra@gmail.com>
2022-05-14 00:38:51 +0300

    Added static pages

--- src/routes.ts
+++ src/routes.ts
@@ -59,6 +59,34 @@ export const routes: RouteDefinition[] = [
     component: lazy(() => import('./pages/about/manifest.mdx'))
   },
   {
+    path: '/about/dogma',
+    component: lazy(() => import('./pages/about/dogma.mdx'))
+  },
+  {
+    path: '/about/guide',
+    component: lazy(() => import('./pages/about/guide'))
+  },
+  {
+    path: '/about/help',
+    component: lazy(() => import('./pages/about/help'))
+  },
+  {
+    path: '/about/partners',
+    component: lazy(() => import('./pages/about/partners.mdx'))
+  },
+  {
+    path: '/about/projects',
+    component: lazy(() => import('./pages/about/projects.mdx'))
+  },
+  {
+    path: '/about/terms-of-use',
+    component: lazy(() => import('./pages/about/terms-of-use'))
+  },
+  {
+    path: '/about/thanks',
+    component: lazy(() => import('./pages/about/thanks'))
+  },
+  {
     path: '/*all',
     component: lazy(() => import('./pages/404'))
   }
