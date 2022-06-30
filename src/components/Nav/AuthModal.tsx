927a99cbc321b3acf006bafdae60e330aace0f29
kvakazyambra <kvakazyambra@gmail.com>
2022-07-01 00:04:16 +0300

    Fixed auth form layout

--- src/components/Nav/AuthModal.tsx
+++ src/components/Nav/AuthModal.tsx
@@ -55,14 +55,14 @@ export default (props: { code?: string; mode?: string }) => {
         ini = { username: '', password: '', email: '' }
         vs = {
           username: Yup.string(),
-          email: Yup.string().required(),
+          email: Yup.string().email().required(),
           password: Yup.string().required(),
         }
         signCheck(emailElement?.value || '')
         break
       case 'forget':
         ini = { email: '' }
-        vs = { email: Yup.string().required() }
+        vs = { email: Yup.string().email().required() }
         break
       case 'reset':
         ini = { code: '' }
@@ -70,7 +70,7 @@ export default (props: { code?: string; mode?: string }) => {
         break
       case 'resend':
         ini = { email: '' }
-        vs = { email: Yup.string().required() }
+        vs = { email: Yup.string().email().email().required() }
         break
       case 'password':
         ini = { password: '', password2: '' }
@@ -177,113 +177,124 @@ export default (props: { code?: string; mode?: string }) => {
           </p>
         </div>
       </div>
-      <Form
-        initialValues={initial()}
-        validation={validation()}
-        onSubmit={async (form) => {console.log(form.values)}}
+      <div class='col-sm-6 auth'>
+        <Form
+          initialValues={initial()}
+          validation={validation()}
+          onSubmit={async (form) => {
+            console.log(form.values)
+          }}
         >
-        <div class='auth__inner'>
-          <h4>{titles[mode()]}</h4>
+          <div class='auth__inner'>
+            <h4>{titles[mode()]}</h4>
 
-          <div class={`auth-subtitle ${mode() === 'forget' ? '' : 'hidden'}`}>
-            <Show
-              when={mode() === 'forget'}
-              fallback={
-                <Show when={mode() === 'reset'}>
-                  {t('Enter the code or click the link from email to confirm')}
-                </Show>
-              }
-            >
-              {t('Everything is ok, please give us your email address')}
-            </Show>
-          </div>
-          <div class={`auth-info`}>
-            <ul>
-              <li class='warn'>{error()}</li>
-            </ul>
-          </div>
-
-          <Show when={false && mode() === 'sign-up'}>
-            <input name='username' autocomplete='username' ref={usernameElement} type='text' placeholder={t('Username')} />
-          </Show>
-          <Show when={mode() !== 'reset' && mode() !== 'password'}>
-            <input name='email' autocomplete='email' ref={emailElement} type='text' placeholder={t('Email')} />
-          </Show>
-
-          <Show when={mode() === 'sign-up' || mode() === 'sign-in' || mode() === 'password'}>
-            <input
-              name='password'
-              autocomplete='current-password'
-              ref={passElement}
-              type='password'
-              placeholder={t('Password')}
-            />
-          </Show>
-          <Show when={mode() === 'reset'}>
-            <input name='resetcode' ref={codeElement} value={props.code} type='text' placeholder={t('Reset code')} />
-          </Show>
-          
-          <Show when={mode() === 'password' || mode() === 'sign-up'}>
-            <input name='password2' ref={pass2Element} type='password' placeholder={t('Password again')} autocomplete="" />
-          </Show>
-          <div>
-            <button class='submitbtn' disabled={auth.handshaking} onClick={localAuth}>
-              {auth.handshaking ? '...' : titles[mode()]}
-            </button>
-          </div>
-
-          <Show when={mode() === 'sign-in'}>
-            <div class='auth-actions'>
-              <a href={''} onClick={() => setMode('forget')}>
-                {t('Forget password?')}
-              </a>
+            <div class={`auth-subtitle ${mode() === 'forget' ? '' : 'hidden'}`}>
+              <Show
+                when={mode() === 'forget'}
+                fallback={
+                  <Show when={mode() === 'reset'}>
+                    {t('Enter the code or click the link from email to confirm')}
+                  </Show>
+                }
+              >
+                {t('Everything is ok, please give us your email address')}
+              </Show>
             </div>
-          </Show>
 
-          <Show when={mode() === 'sign-in' || mode() === 'sign-up'}>
-            <div class='social-provider'>
-              <div class='providers-text'>{t('Or continue with social network')}</div>
-              <div class='social'>
-                <a href={''} class='facebook-auth' onClick={() => oauth('facebook')}>
-                  <Icon name='facebook' />
-                </a>
-                <a href={''} class='google-auth' onClick={() => oauth('google')}>
-                  <Icon name='google' />
-                </a>
-                <a href={''} class='vk-auth' onClick={() => oauth('vk')}>
-                  <Icon name='vk' />
-                </a>
-                <a href={''} class='github-auth' onClick={() => oauth('github')}>
-                  <Icon name='github' />
+            <Show when={error()}>
+              <div class={`auth-info`}>
+                <ul>
+                  <li class='warn'>{error()}</li>
+                </ul>
+              </div>
+            </Show>
+
+            <Show when={false && mode() === 'sign-up'}>
+              <input name='username' autocomplete='username' ref={usernameElement} type='text'
+                     placeholder={t('Username')}/>
+            </Show>
+            <Show when={mode() !== 'reset' && mode() !== 'password'}>
+              <input name='email' autocomplete='email' ref={emailElement} type='text'
+                     placeholder={t('Email')}/>
+            </Show>
+
+            <Show when={mode() === 'sign-up' || mode() === 'sign-in' || mode() === 'password'}>
+              <input
+                name='password'
+                autocomplete='current-password'
+                ref={passElement}
+                type='password'
+                placeholder={t('Password')}
+              />
+            </Show>
+            <Show when={mode() === 'reset'}>
+              <input name='resetcode' ref={codeElement} value={props.code} type='text'
+                     placeholder={t('Reset code')}/>
+            </Show>
+
+            <Show when={mode() === 'password' || mode() === 'sign-up'}>
+              <input name='password2' ref={pass2Element} type='password' placeholder={t('Password again')}
+                     autocomplete=""/>
+            </Show>
+            <div>
+              <button class='button submitbtn' disabled={auth.handshaking} onClick={localAuth}>
+                {auth.handshaking ? '...' : titles[mode()]}
+              </button>
+            </div>
+
+            <Show when={mode() === 'sign-in'}>
+              <div class='auth-actions'>
+                <a href={''} onClick={() => setMode('forget')}>
+                  {t('Forget password?')}
                 </a>
               </div>
-            </div>
-          </Show>
+            </Show>
 
-          <div class='auth-control'>
-            <div classList={{ show: mode() === 'sign-up' }}>
+            <Show when={mode() === 'sign-in' || mode() === 'sign-up'}>
+              <div class='social-provider'>
+                <div class='providers-text'>{t('Or continue with social network')}</div>
+                <div class='social'>
+                  <a href={''} class='facebook-auth' onClick={() => oauth('facebook')}>
+                    <Icon name='facebook'/>
+                  </a>
+                  <a href={''} class='google-auth' onClick={() => oauth('google')}>
+                    <Icon name='google'/>
+                  </a>
+                  <a href={''} class='vk-auth' onClick={() => oauth('vk')}>
+                    <Icon name='vk'/>
+                  </a>
+                  <a href={''} class='github-auth' onClick={() => oauth('github')}>
+                    <Icon name='github'/>
+                  </a>
+                </div>
+              </div>
+            </Show>
+
+            <div class='auth-control'>
+              <div classList={{show: mode() === 'sign-up'}}>
               <span class='auth-link' onClick={() => setMode('sign-in')}>
                 {t('I have an account')}
               </span>
-            </div>
-            <div classList={{ show: mode() === 'sign-in' }}>
+              </div>
+              <div classList={{show: mode() === 'sign-in'}}>
               <span class='auth-link' onClick={() => setMode('sign-up')}>
                 {t('I have no account yet')}
               </span>
-            </div>
-            <div classList={{ show: mode() === 'forget' }}>
+              </div>
+              <div classList={{show: mode() === 'forget'}}>
               <span class='auth-link' onClick={() => setMode('sign-in')}>
                 {t('I know the password')}
               </span>
-            </div>
-            <div classList={{ show: mode() === 'reset' }}>
+              </div>
+              <div classList={{show: mode() === 'reset'}}>
               <span class='auth-link' onClick={() => setMode('resend')}>
                 {t('Resend code')}
               </span>
+              </div>
             </div>
           </div>
-        </div>
-      </Form>
+        </Form>
+      </div>
     </div>
   )
 }
