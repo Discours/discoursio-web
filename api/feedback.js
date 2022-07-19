5e578302efbdc07d2be0c2bd5ef2f64df8aaf608
tonyrewin <anton.rewin@gmail.com>
2022-07-19 16:37:32 +0300

    serverless api

--- api/feedback.js
+++ api/feedback.js
@@ -1,25 +1,23 @@
-const nodemailer = require('nodemailer')
-
-const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: 25,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
-})
+const mailgun = require('mailgun-js')({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN })
+const MailComposer = require("nodemailer/lib/mail-composer");
 
 export default async function handler(req, res) {
   const { contact, message } = req.query
   try {
-    await transporter.sendMail({
+    const mail = new MailComposer({
       to: 'welcome@discours.io',
       from: contact + ' <robot@discours.io>',
       subject: `Feedback: ${contact}`,
-      text: message
+      body: message
+    }).compile()
+    mail.build((err, mail) => {
+      if (err) res.status(400).json(err)
+      else mailgun.messages().send(mail, (err, message) => {
+        if (err) res.status(400).json(err)
+        else res.status(200).json({ message }) // message is response from mailgun
+      })
     })
-    res.status(200).json({ message: 'Successfully send your message!' })
   } catch (error) {
-    res.status(400).json({ message: 'You submit an incorrect mail address!' })
+    res.status(400).json(error)
   }
 }
--- api/newsletter.js
+++ api/newsletter.js
@@ -2,7 +2,7 @@ const mailgun = require('mailgun-js')({
   apiKey: process.env.MAILGUN_API_KEY,
   domain: process.env.MAILGUN_DOMAIN
 })
-const list = mailgun.lists('services@discours.io')
+const list = mailgun.lists(process.env.MAILGUN_LIST_ID) // 'services@discours.io'
 
 export default async function handler(req, res) {
   const { email } = req.body.data
